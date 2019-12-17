ead3228cc909a72d91bec436200ff6ba42b27428
zmeriksen <zmeriksen@gmail.com>
2019-12-16 20:06:02 -0600

    Updated to show Units

--- AOEII/AOEII/Views/Unit/UnitDetailView.swift
+++ AOEII/AOEII/Views/Unit/UnitDetailView.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2019 oneleif. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SwiftUIKit
+
+class UnitDetailView: UIView {
+    
+    private var unit: Unit
+    
+    init(_ unit: Unit) {
+        self.unit = unit
+        super.init(frame: .zero)
+        
+        log.debug.entry {
+            "Init AOEII UnitDetailView(civ: \(unit.name ?? ""))"
+        }
+        
+        Navigate.shared.set(title: unit.name ?? "")
+        
+        embed {
+            VStack(distribution: .equalSpacing) {
+                [
+                    VStack {
+                        [
+                            Label.title1(unit.name ?? ""),
+                            Label.title3("Expansion: \(unit.expansion ?? "")"),
+                            Label.title3("Age: \(unit.age ?? "")")
+                        ]
+                    }
+                    .frame(height: 100),
+                    
+                   
+                    Spacer()
+                    
+                ]
+            }
+        }
+        .debug()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
